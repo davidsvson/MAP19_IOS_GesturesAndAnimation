8d761ce8447901c7e39ac668c0c8ed5a12afbba5
David Svensson <davidsvson@gmail.com>
2020-01-17 13:05:29 +0100

    Added gestures and animated label

--- GesturesAndAnimation/ViewController.swift
+++ GesturesAndAnimation/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var doubleTapRecognizer: UITapGestureRecognizer!
+    @IBOutlet var singleTapRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var gestureNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        singleTapRecognizer.require(toFail: doubleTapRecognizer)
     }
+    
+    @IBAction func singleTap(_ sender: UITapGestureRecognizer) {
+        showGesture(name: "Tap")
+        
+    }
+    
+    @IBAction func doubleTap(_ sender: UITapGestureRecognizer) {
+        showGesture(name: "Double Tap")
+    }
+    
+    
+    
+    func showGesture(name: String) {
+        gestureNameLabel.text = name
+        
+        UIView.animate(withDuration: 1.0, animations: {self.gestureNameLabel.alpha = 1.0},
+                       completion: makeLabelInviceble(finished:) )
+    }
+    
 
-
+    func makeLabelInviceble(finished: Bool) {
+        UIView.animate(withDuration: 1.0, animations: {self.gestureNameLabel.alpha = 0.0})
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            showGesture(name: "Shake")
+        }
+    }
+    
 }
 
