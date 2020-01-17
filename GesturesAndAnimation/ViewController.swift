145e06088e1e5ba55a2162213864b35dae191742
David Svensson <davidsvson@gmail.com>
2020-01-17 13:09:29 +0100

    added more comments

--- GesturesAndAnimation/ViewController.swift
+++ GesturesAndAnimation/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // let tap wait for doubletap to fail
         singleTapRecognizer.require(toFail: doubleTapRecognizer)
     }
     
